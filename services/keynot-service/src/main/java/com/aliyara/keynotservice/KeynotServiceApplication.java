b04783c833516b43e3782fe43a37d94436f3112f
Ali Yara <ali.yara.fr@gmail.com>
2026-03-02 10:14:17 +0000

    initialise keynot-service

---
+++ services/keynot-service/src/main/java/com/aliyara/keynotservice/KeynotServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aliyara.keynotservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeynotServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeynotServiceApplication.class, args);
+	}
+
+}
